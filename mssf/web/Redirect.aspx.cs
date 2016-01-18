9f86208760d02ccea6d6061001764626425adb8e
fraenkthetaenk <fsteiner@hm.edu>
2016-01-18 19:23:28 +0100

    up
    a

---
+++ mssf/web/Redirect.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web
+{
+	public partial class Redirect1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            if(Session.Count == 0)
+            {
+                Response.Redirect("default.aspx");
+            }
+            else
+            {
+                bll.clsUser user = (bll.clsUser)Session["Current User"];
+                switch (user.Role)
+                {
+                    case 0:
+                        Response.Redirect("Costumer/CostumerDefault.aspx");
+                        break;
+                    case 1:
+                        Response.Redirect("Manager/ManagerDefault.aspx");
+                        break;
+                    case 2:
+                        Response.Redirect("Worker/WorkerDeafult.aspx");
+                        break;
+                }
+            }
+              
+		}
+	}
+}
